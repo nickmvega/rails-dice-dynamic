4130d32740f8adb7077c0695ebcbbb895fe8768c
Nicholas Vega <122232969+nickmvega@users.noreply.github.com>
2024-10-28 20:12:24 +0000

    Hw

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
-  skip_forgery_protection
+  def home
+    render({ template: "layouts/home" })
+  end
+
+  def flexible 
+    @num_dice = params.fetch("number_of_dice").to_i
+
+    @sides = params.fetch("how_many_sides").to_i
+
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+
+      @rolls.push(die)
+    end
+
+    render({ template: "layouts/flexible" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-
+  get("/", { :controller => "application", :action => "home"})
+  get("/dice/:number_of_dice/:how_many_sides", { :controller => "application", :action => "flexible"})
 end
